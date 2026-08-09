653fe26aaead72a872383d55ccd98213563f4855
Харке Денис <d.harke@yandex.ru>
2026-08-09 20:56:13 +0300

    git init

---
+++ webpack.config.mjs
@@ -0,0 +1,15 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+    clean: true,
+  },
+};
