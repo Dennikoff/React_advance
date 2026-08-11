1d58133038b4326b0b3d5acf4698cd9b26a27f58
Харке Денис <d.harke@yandex.ru>
2026-08-11 11:34:43 +0300

    add routing and suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from "react";
-import { Counter } from "./Counter";
 import "./index.scss";
+import { Route, Routes, Link } from "react-router-dom";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { Suspense } from "react";
 
 export const App = () => {
   return (
     <div className="app">
-      fdsadasdddasdasd
-      <Counter />
+      <div>
+        <Link to="/">Главная страница</Link>
+        <Link to="/about">О сайте</Link>
+      </div>
+      <Suspense fallback={<>Loading...</>}>
+        <Routes>
+          <Route path={"/"} element={<MainPageAsync />} />
+          <Route path={"/about"} element={<AboutPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { createRoot } from "react-dom/client";
-import { Counter } from "./Counter";
 import { App } from "./App";
 import { StrictMode } from "react";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 );
