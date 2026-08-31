0f6ad5e41452197f2a7c940fbb7daf18b6fcf9b9
Харке Денис <d.harke@yandex.ru>
2026-08-31 14:39:11 +0300

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <ThemeProvider>
-        <App />
+        <App /> 
       </ThemeProvider>
     </BrowserRouter>
   </StrictMode>,
